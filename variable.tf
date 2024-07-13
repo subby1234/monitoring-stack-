c3676a96b77057a5b5cc3875cacdfdf394bad45f
subby1234 <suruuuu_baba@yahoo.com>
2024-07-14 00:18:03 +0100

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,21 @@
+variable "access_key" {
+  default = ""
+}
+
+variable "secret_key" {
+  default = ""
+}
+
+variable "iamuser" {
+  type    = list(any)
+  default = ["akintoye", "akinsola", "kay", "oluyemi", "olasubomi", "dotun"]
+}
+
+variable "ssh_key" {
+  default = ""
+}
+
+variable "bucket_name" {
+  default = "lcd-wedsite"
+  type    = string
+}
