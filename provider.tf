046999d1fd6e6e48fbff5d5f027f4c26a97b3f2d
subby1234 <suruuuu_baba@yahoo.com>
2024-07-14 00:15:42 +0100

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.48.0"
+    }
+    gitlab = {
+      source  = "gitlabhq/gitlab"
+      version = "15.7.1"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+provider "gitlab" {
+  token = var.gitlab_token
+}
